f927c1821db20964c891c7eaba580d693fd18a01
Youcef Aissat <y.aissat@gmail.com>
2023-05-23 23:24:32 +0200

    create Put route

--- userHandlers.js
+++ userHandlers.js
@@ -46,8 +46,31 @@ const getUsers = (req, res) => {
       });
   };
   
+  const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, director, year, color, duration } = req.body;
+  
+    database
+      .query(
+        "update Users set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+        [title, director, year, color, duration, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error editing the User");
+      });
+  };
+
   module.exports = {
     getUsers,
     getUserById,
     postUser, 
+    updateUser,
   };
